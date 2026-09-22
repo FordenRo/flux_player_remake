a1dc310f7980d1c0e5db82808a7dbece5a34420d
FordenRo <FordenRo@yandex.ru>
2026-09-22 22:31:32 +0500

    Added queue reordering, context menu entries and action buttons

--- lib/ui/pages/queue_page.dart
+++ lib/ui/pages/queue_page.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_context_menu/flutter_context_menu.dart';
 
 import '../../core/services/audio_player.dart';
+import '../../core/utils/move_element.dart';
+import '../widgets/track_item.dart';
 import '../widgets/track_list_view.dart';
 
 class QueuePage extends StatefulWidget {
@@ -40,12 +43,35 @@ class _QueuePageState extends State<QueuePage>
       tracks: tracks,
       onTrackPlayed: (index) =>
           audioPlayer.setQueue(tracks, index: index, play: true),
-      trackActionButtons: const [
-        // TrackActionButton(
-        //   icon: Icons.close_rounded,
-        //   iconSize: 20,
-        //   onPressed: ,
-        // ),
+      onTrackMoved: (oldIndex, newIndex) {
+        audioPlayer.setQueue(
+          tracks..move(oldIndex, newIndex),
+          index: audioPlayer.currentIndex,
+          load: false,
+        );
+        if (audioPlayer.currentIndex! == oldIndex) {
+          audioPlayer.setIndex(newIndex, load: false);
+        } else if (audioPlayer.currentIndex! < oldIndex &&
+            audioPlayer.currentIndex! >= newIndex) {
+          audioPlayer.setIndex(audioPlayer.currentIndex! + 1, load: false);
+        } else if (audioPlayer.currentIndex! > oldIndex &&
+            audioPlayer.currentIndex! <= newIndex) {
+          audioPlayer.setIndex(audioPlayer.currentIndex! - 1, load: false);
+        }
+      },
+      menuEntriesBuilder: (context, index) => [
+        MenuItem(
+          label: const Text('Убрать'),
+          onSelected: (_) => audioPlayer.removeFromQueue(index),
+        ),
+      ],
+      trackActionButtonsBuilder: (context, index, child) => [
+        ...child,
+        TrackActionButton(
+          icon: Icons.close_rounded,
+          iconSize: 20,
+          onPressed: () => audioPlayer.removeFromQueue(index),
+        ),
       ],
     );
   }
